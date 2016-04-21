7aa275c884b856bad156f62b9acafab2fb600d1b
netsudo <netsudo@users.noreply.github.com>
2016-04-20 21:34:13 -0700

    initial commitment

---
+++ pyddit.py
@@ -0,0 +1,38 @@
+import requests
+import lxml.html
+import sys
+import time
+
+class Request:
+
+    def __init__(self):
+        self.url = ''
+        self.user_agent = {'user-agent': 'Mozilla/5.0 \
+                            (compatible; MSIE 10.0; Windows NT 6.2; ARM; Trident/6.0)'}
+
+    def request(self):
+        r = requests.get(self.url, headers=self.user_agent)
+
+        return r.text
+
+    def titleParse(self):
+        tree = lxml.html.fromstring(self.request())
+        titles = tree.xpath('//*/div[2]/p[1]/a/text()')
+
+        return titles
+
+    def urlParse(self):
+        tree = lxml.html.fromstring(self.request())
+        urls = tree.xpath('//*/div[2]/p[1]/a/@href')
+
+        return urls
+
+    def upVotes(self):
+        tree = lxml.html.fromstring(self.request())
+        votes = tree.xpath('//*/div[1]/div[3]/text()')
+
+        return votes
+
+r = Request()
+r.url = 'https://www.reddit.com/r/all'
+print r.titleParse()
