4fa6d888601d7de73af44b7d270111308070da9b
netsudo <netsudo@users.noreply.github.com>
2016-04-24 14:06:39 -0700

    Cleanup

--- pyddit.py
+++ pyddit.py
@@ -6,6 +6,10 @@ from multiprocessing.dummy import Pool as ThreadPool
 
 class Request:
 
+    pool = ThreadPool(4)
+    xpaths = ['//*/div[2]/p[1]/a/text()', '//*/div[2]/p[1]/a/@href',\
+    '//*/div[1]/div[3]/text()']
+
     def __init__(self):
         self.url = ''
         self.user_agent = {'user-agent': 'Mozilla/5.0 \
@@ -16,19 +20,16 @@ class Request:
 
         return r.text
 
-def titleParse(url, pool, xpaths):
-    tree = lxml.html.fromstring(url)
+    def titleParse(self):
+        tree = lxml.html.fromstring(self.request())
 
-    results = pool.map(tree.xpath, xpaths)
-    for title, url, votes in zip(results[0], results[1], results[2]):
-        print votes + " " + title + " " + "\n" + url
-    pool.close()
-    pool.join()
+        results = self.pool.map(tree.xpath, self.xpaths)
+        for title, url, votes in zip(results[0], results[1], results[2]):
+            print votes + " " + title + " " + "\n" + url
+        self.pool.close()
+        self.pool.join()
 
 if __name__ == '__main__':
     r = Request()
     r.url = 'https://www.reddit.com/r/all'
-    url = r.request()
-    pool = ThreadPool(6)
-    xpaths = ['//*/div[2]/p[1]/a/text()', '//*/div[2]/p[1]/a/@href', '//*/div[1]/div[3]/text()']
-    titleParse(url, pool, xpaths)
+    r.titleParse()
