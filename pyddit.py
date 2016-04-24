0d26c82a1652d6ee18f5cb9ddbff39bd7f906151
netsudo <netsudo@users.noreply.github.com>
2016-04-23 18:22:16 -0700

    Commiting for historical data, want to refer to this in the future.

--- pyddit.py
+++ pyddit.py
@@ -3,6 +3,8 @@ import lxml.html
 import sys
 import time
 from threading import Thread
+from itertools import *
+from multiprocessing import Process, Queue
 
 class ThreadWithReturnValue(Thread):
     def __init__(self, group=None, target=None, name=None,
@@ -31,19 +33,19 @@ class Request:
 
         return r.text
 
-def titleParse():
+def titleParse(url, que1):
     tree = lxml.html.fromstring(r.request())
     titles = tree.xpath('//*/div[2]/p[1]/a/text()')
 
     return titles
 
-def urlParse():
+def urlParse(url, que2):
     tree = lxml.html.fromstring(r.request())
     urls = tree.xpath('//*/div[2]/p[1]/a/@href')
 
     return urls
 
-def upVotes():
+def upVotes(url, que3):
     tree = lxml.html.fromstring(r.request())
     votes = tree.xpath('//*/div[1]/div[3]/text()')
 
@@ -52,9 +54,13 @@ def upVotes():
 if __name__ == '__main__':
     r = Request()
     r.url = 'https://www.reddit.com/r/all'
-    t1 = ThreadWithReturnValue(target = titleParse); t2 = ThreadWithReturnValue(target=urlParse); t3 = ThreadWithReturnValue(target=upVotes)
+    url = r.request()
+    queue1=Queue(); queue2=Queue(); queue3=Queue()
+    p1 = Process(target = titleParse, args=(url,queue1,)); p2= Process(target=urlParse, args=(url, queue2)); p3 = Process(target=upVotes, args=(url,queue3,))
+    p1.start(); p2.start(); p3.start()
 
-    t1.start(); t2.start(); t3.start()
+    #t1.start(); t2.start(); t3.start();t1.join();t2.join();t3.join()
 
-    for title, url, votes in zip(t1.join(), t2.join(), t3.join()):
+    for title, url, votes in izip(queue1.get(), queue2.get(), queue3.get()):
         print votes + " " + title + " " + "\n" + url
+        p1.join(); p2.join(); p3.join()
